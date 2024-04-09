683598abfa260bdc45c9a2f52fac817d1beff9ec
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-10 00:52:30 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -21,8 +21,8 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
+    buttons = [[InlineKeyboardButton(text="𝙅𝙤𝙞𝙣 𝘾𝙝𝙖𝙣𝙣𝙚𝙡", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    text = "**Sᴏʀʀy Dᴜᴅᴇ, Yᴏᴜ ɴᴇᴇᴅ ᴛᴏ Jᴏɪɴ ᴏᴜʀ Cʜᴀɴɴᴇʟ ғɪʀsᴛ ᴏɴʟʏ ᴀғᴛᴇʀ ᴛʜᴀᴛ Yᴏᴜ ᴄᴀɴ ᴀʙʟᴇ ᴛᴏ ᴀᴄᴄᴇss ᴍᴇ.**"
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
         if user.status == enums.ChatMemberStatus.BANNED:                                   
