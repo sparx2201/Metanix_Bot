1dfd9e66abad3684666b92eb421b9c982dd1e84f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-09 16:47:06 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -19,7 +19,7 @@ class Database:
             prefix=None,
             suffix=None,
             metadata=False,
-            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """
+            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Created By:- 𝘈𝘑" -metadata author="𝘈𝘑" -metadata:s:s title="Subtitled By :- @MetaNiXbot" -metadata:s:a title="By :- @MetaNiXbot" -metadata:s:v title="By:- 𝘈𝘑" """
         )
 
     async def add_user(self, b, m):
